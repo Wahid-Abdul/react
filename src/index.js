1146874a751f68b8a205bc823985f3c5966cf7a0
wahid_abdul <abdulwahid.ag@marketsimplified.com>
2018-09-03 17:48:23 +0530

    CLock works

--- src/index.js
+++ src/index.js
@@ -60,6 +60,8 @@ class Game extends React.Component {
 
 // ========================================
 
+
+// My components
 class Login extends React.Component {
   render() {
     return (
@@ -71,17 +73,54 @@ class Login extends React.Component {
     )
   }
 }
+// My components end
 
+// Clock compoenent starts
+class Clock extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+    alert(" will unmount")
+
+  }
+
+  tick(){
+    this.setState({
+      date: new Date()
+    })
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+
+// Clock compoenent ends
 
 ReactDOM.render(
   <div>
-    <Game />  <Login />
+    <Game /> 
+    <Login />
+    <Clock />
   </div>,
   document.getElementById('root')
 );
-
-
-  // ReactDOM.render(
-  //   <Login />,
-  //   document.getElementById('root')
-  // );
